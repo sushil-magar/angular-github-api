59d6e8d929cb261ff3ae1da223680894dae5dab3
Sushil Magar <sushil.magar@springboardauto.com>
2018-06-09 20:55:31 -0700

    Express setup

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+require('dotenv').config({silent: true});
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const obj = {
-  one: 1,
-};
+const path = require('path');
+
+// const db = require('./db/config');
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.resolve(__dirname + './dist')));
+
+app.get('/', (req, res) => {
+  res.send('Hello..Server started!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
