d5f454a5553f863cd13a33943dff35b83e482935
Sushil Magar <sushil.magar@springboardauto.com>
2018-06-09 20:18:11 -0700

    Setup eslint

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const obj = {
+  one: 1,
+};
